486a06c8f77ac50fa7deb275dab29929156d9852
Andrew Young <ayoung21@my.westga.edu>
2020-10-14 13:57:53 -0400

    added initial unit test (example test case)

---
+++ CovidAnalysisUnitTest/CovidCaseTest/ToString.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Globalization;
+using Covid19Analysis.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CovidAnalysisUnitTest
+{
+    [TestClass]
+    public class CovidCaseTest
+    {
+        private const string DateFormat = "yyyyMMdd";
+
+        [TestMethod]
+        public void TestConstructor()
+        {
+            var date = DateTime.ParseExact("20200816", DateFormat, CultureInfo.InvariantCulture);
+            var covidCase = new CovidCase("GA", date);
+            var expected = "8/16/2020 GA : [+Increase] 0 [-increase] 0 [death increase] 0 [hospitalized increase] 0";
+            var result = covidCase.ToString();
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
