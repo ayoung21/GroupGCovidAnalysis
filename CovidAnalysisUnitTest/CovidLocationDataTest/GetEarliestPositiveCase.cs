cba1af5eed09560007f5bb8220fb8221840c5dad
Andrew Young <ayoung21@my.westga.edu>
2020-10-18 15:56:13 -0400

    added additional unit tests

---
+++ CovidAnalysisUnitTest/CovidLocationDataTest/GetEarliestPositiveCase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Covid19Analysis.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CovidAnalysisUnitTest
+{
+    [TestClass]
+    public class TestingGetEarliestPositiveCase
+    {
+        [TestMethod]
+        public void TestGetEarliestPositiveCaseReturnNull()
+        {
+            CovidLocationData covidLocationData = new CovidLocationData("GA");
+
+            Assert.IsNull(covidLocationData.GetEarliestPositiveCase());
+        }
+
+        [TestMethod]
+        public void TestGetEarliestPositiveCase()
+        {
+            CovidLocationData covidLocationData = new CovidLocationData("GA");
+
+            covidLocationData.AddCovidCase(new CovidCase("GA", new DateTime(2016, 2, 15))
+            {
+                PositiveIncrease = 125
+            });
+            covidLocationData.AddCovidCase(new CovidCase("GA", new DateTime(2016, 2, 10))
+            {
+                PositiveIncrease = 125
+            });
+            covidLocationData.AddCovidCase(new CovidCase("GA", new DateTime(2016, 2, 12))
+            {
+                PositiveIncrease = 125
+            });
+
+            var expected = "2/10/2016";
+
+            var covidCase = covidLocationData.GetEarliestPositiveCase();
+            var result = covidCase.Date.ToShortDateString();
+
+            Assert.AreEqual(expected, result, true);
+        }
+    }
+}
---
+++ CovidAnalysisUnitTest/CovidLocationDataTest/GetHighestHospitalization.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Covid19Analysis.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CovidAnalysisUnitTest
+{
+    [TestClass]
+    public class TestingGetHighestHospitalizations
+    {
+        [TestMethod]
+        public void TestGetHighestHospitalizationReturnNull()
+        {
+            CovidLocationData covidLocationData = new CovidLocationData("GA");
+
+            Assert.IsNull(covidLocationData.GetHighestHospitalization());
+        }
+
+        [TestMethod]
+        public void TestGetHighestHospitalizationCase()
+        {
+            CovidLocationData covidLocationData = new CovidLocationData("GA");
+
+            covidLocationData.AddCovidCase(new CovidCase("GA", new DateTime(2015, 2, 15))
+            {
+                HospitalizedIncrease = 50
+            });
+
+            covidLocationData.AddCovidCase(new CovidCase("GA", new DateTime(2016, 2, 15))
+            {
+                HospitalizedIncrease = 125
+            });
+
+            int expected = 125;
+            int result = covidLocationData.GetHighestHospitalization().HospitalizedIncrease;
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
