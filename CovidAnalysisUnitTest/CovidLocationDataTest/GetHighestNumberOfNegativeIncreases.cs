6e322ffa781695a0c79b7f738ea4e7c37a5c8c66
Andrew Young <ayoung21@my.westga.edu>
2020-11-08 19:36:42 -0500

    added final unit test for requirement

---
+++ CovidAnalysisUnitTest/CovidLocationDataTest/GetHighestNumberOfNegativeIncreases.cs
@@ -0,0 +1,55 @@
+﻿
+using Covid19Analysis.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CovidAnalysisUnitTest
+{
+    [TestClass]
+    public class TestingGetHighestNumberOfNegativeIncreases
+    {
+        [TestMethod]
+        public void TestNull()
+        {
+            CovidLocationData covidLocationData = new CovidLocationData("GA");
+
+            Assert.IsNull(covidLocationData.GetHighestNumberOfNegativeIncreases());
+        }
+
+        [TestMethod]
+        public void TestHighestWithOnlyOneCase()
+        {
+            CovidLocationData covidLocationData = new CovidLocationData("GA");
+
+            covidLocationData.AddCovidCase(new CovidCase("GA", new DateTime(2015, 2, 15)) {
+                NegativeIncrease = 0
+            });
+
+            var expected = 0;
+            var result = covidLocationData.GetHighestNumberOfNegativeIncreases().NegativeIncrease;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestHighestWithManyCases()
+        {
+            CovidLocationData covidLocationData = new CovidLocationData("GA");
+
+            covidLocationData.AddCovidCase(new CovidCase("GA", new DateTime(2015, 2, 15))
+            {
+                NegativeIncrease = 0
+            });
+
+            covidLocationData.AddCovidCase(new CovidCase("GA", new DateTime(2016, 2, 15))
+            {
+                NegativeIncrease = 5
+            });
+
+            var expected = 5;
+            var result = covidLocationData.GetHighestNumberOfNegativeIncreases().NegativeIncrease;
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
